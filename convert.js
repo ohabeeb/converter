30b928ed5d2e66d17841d2e04119586b673d0c0d
ohabeeb <51259360+ohabeeb@users.noreply.github.com>
2020-06-14 20:30:42 +0100

    Update convert.js

--- convert.js
+++ convert.js
@@ -4,6 +4,34 @@
 
     input.addEventListener("keyup",myResult);
     result.addEventListener("keyup",myResult);
+     result2.addEventListener("keyup",myResult);
+    
+function myResult(){
+	 //result.value = input.value;
+	 //input.value = result.value;
+
+     if (/*result.value = */input.value != "" ) {  
+ 		result.value = input.value *0.01;
+
+ 		var result2 = document.getElementById("result2").innerHTML
+      = input.value + " CM = " +  result.value + " M "
+
+     } else if (result.value != "" /*input.value*/) {
+
+         input.value = result.value * 100;
+         var result2 = document.getElementById("result2").innerHTML
+             = result.value + " M = " + input.value + " CM "
+ 	
+ 	}
+ };
+
+ //****************************************************************//
+/* var input = document.getElementById("input");
+	var result = document.getElementById("result");
+	var result2 = document.getElementById("result2");
+
+    input.addEventListener("keyup",myResult);
+    result.addEventListener("keyup",myResult);
     // result2.addEventListener("keyup",myResult);
     
 function myResult(){
@@ -22,7 +50,7 @@ function myResult(){
  	
  	}
  };
-
+*/
  
 
 
